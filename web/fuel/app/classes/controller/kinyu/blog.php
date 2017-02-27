36318db55c7ab45ad18837df67fa83bfee4edc98
Keita Komori <komori@toetheline-net.com>
2017-02-27 14:51:39 +0900

    Fix

--- web/fuel/app/classes/controller/kinyu/blog.php
+++ web/fuel/app/classes/controller/kinyu/blog.php
@@ -15,13 +15,10 @@ class Controller_Kinyu_Blog extends Controller_Kinyubase
 		$this->template->description = 'きんゆう女子。は、なかなか聞けないお金の話。 先延ばしにしがちなお金の計画。 私には無関係と思っている金融の話など、お金に関する様々な情報を配信しています。';
 		$this->template->ogimg = 'https://kinyu-joshi.jp/images/kinyu-logo.png';
 		//template
+
     $this->data['top_blogs'] = Blogs::lists(1, 5, true);
     $this->data['specials'] = Blogs::lists(1, 5, true, 'special');
     $this->data['specials02'] = Blogs::lists02(1, 4, true, 'special');
-    //$this->template->header = View::forge('kinyu/template/header-area.smarty', $this->data);
-    //$this->template->navi_contents = View::forge('kinyu/template/navi_contents.smarty', $this->data);
-    //$this->template->footer = View::forge('kinyu/template/footer-area.smarty', $this->data);
-    //$this->template->contents_after_area = View::forge('kinyu/template/contents-after.smarty', $this->data);
     $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
     $this->template->pc_side = View::forge('kinyu/common/pc_side.smarty', $this->data);
     $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
