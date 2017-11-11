7ae27d5e8d30aa6f1a861bd45d948e005c6f5130
kuniiskywalker <kuniiskywalker@gmail.com>
2017-11-11 12:50:03 +0900

    add docker

--- web/fuel/app/config/development/db.php
+++ web/fuel/app/config/development/db.php
@@ -5,13 +5,14 @@
  * This environment is primarily used by unit tests, to run on a controlled environment.
  */
 
+
 return array(
 	'default' => array(
     'charset' => 'utf8',
 		'connection'  => array(
-                        'dsn'        => 'mysql:host=dbsrv-sundaylunch-cluster-1.cluster-cqeoc0c4hmvu.ap-northeast-1.rds.amazonaws.com;dbname=kinyujoshi',
+                        'dsn'        => 'mysql:host=db;dbname=kinyujoshi',
                         'username'   => 'root',
-                        'password'   => ']8Dvung|',
+                        'password'   => 'pass',
 		),
 		'profiling' => true,
 	),
