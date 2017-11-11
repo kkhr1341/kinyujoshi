972c4609bc16130c3b007e515993e7d4059cded3
kuniiskywalker <kuniiskywalker@gmail.com>
2017-11-11 12:56:47 +0900

    gitにふくめちゃいけないやつを削除＆git管理対象外へ変更

--- web/fuel/app/logs/2017/02/15.php
+++
@@ -1,3 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2017-02-15 12:51:07 --> Notice - Undefined index: navigation in /Users/komokei/git/SL/kinyujyoshi/web/fuel/app/tmp/Smarty/templates_c/b96219b7aaa0c8ecfab2219df8e42644cbc968e3_0.file.main.smarty.php on line 79
