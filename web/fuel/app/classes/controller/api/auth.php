44a6c94be1fd0f070ac39d4d184bfe9b75e5b33c
kuniiskywalker <kuniiskywalker@gmail.com>
2017-12-03 18:45:31 +0900

    no message

---
+++ web/fuel/app/classes/controller/api/auth.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The Auth Api Controller.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+
+use \Model\Login;
+use \Model\Register;
+
+class Controller_Api_Auth extends Controller_Base
+{
+	public function action_login() {
+        $val = Login::validate();
+        if (!$val->run()) {
+            $this->error('ログインに失敗しました');
+        }
+        $params = $val->validated();
+        $res = \Auth::login($params['email'], $params['password']);
+        if (!$res) {
+            $this->error('ログインに失敗しました');
+        }
+        return $this->ok('success');
+	}
+
+    public function action_regist() {
+        $val = Register::validate();
+        if (!$val->run()) {
+            return $this->error('登録に失敗しました');
+        }
+        try {
+            $params = $val->validated();
+            Auth::create_user(
+                Str::random('alnum', 16),
+                $params['password'],
+                $params['email']
+            );
+            Auth::login($params['email'], $params['password']);
+        } catch (Exception $e) {
+            $this->error('登録に失敗しました');
+        }
+        return $this->ok('success');
+    }
+}
