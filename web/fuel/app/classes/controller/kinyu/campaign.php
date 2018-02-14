c2d1dd92e932084acd724a1fb7e9e4901f1bb6f8
toetheline03 <komori@sundaylunch.jp>
2018-02-14 18:15:59 +0900

    [master] ひみつの大女子会調整

--- web/fuel/app/classes/controller/kinyu/campaign.php
+++ web/fuel/app/classes/controller/kinyu/campaign.php
@@ -103,7 +103,7 @@ class Controller_Kinyu_Campaign extends Controller_Kinyubase
        die('このページを見るにはログインが必要です');
     }
 
-    $this->template->title = '第1回 週末投資宣言♪｜きんゆう女子。';
+    $this->template->title = '第2回 東証秘密の大女子会｜きんゆう女子。';
     $this->template->ogimg = 'https://kinyu-joshi.jp/images/og-conference.jpg';
     $this->template->description = '宣誓！わたしたちは、投資の本質を知り正々堂々とおかねを増やすことを誓います！わたしたちは、週末時間をゆたかで楽しい人生にするために。正しいおかねの知識と意識を身につけ前向きに投資をしていきます。投資の一歩手前の準備をしっかりしてすてきな投資家になり、経済に参加します。このイベントでは、その誓いを宣言し第一歩を踏み出すきっかけを自ら作ります。';
 
---
+++ web/public/assets/js/kinyu/campaign/conference.js
@@ -0,0 +1,19 @@
+$(function() {
+  program_el1 = $(".joshikai-table .joshikai-table-el1 td").height();
+  $('.joshikai-table .joshikai-table-el1 th').css('height', program_el1);
+
+  program_el2 = $(".joshikai-table .joshikai-table-el2 td").height();
+  $('.joshikai-table .joshikai-table-el2 th').css('height', program_el2);
+
+  program_el3 = $(".joshikai-table .joshikai-table-el3 td").height();
+  $('.joshikai-table .joshikai-table-el3 th').css('height', program_el2);
+
+  program_el4 = $(".joshikai-table .joshikai-table-el4 td").height();
+  $('.joshikai-table .joshikai-table-el4 th').css('height', program_el2);
+
+  program_el5 = $(".joshikai-table .joshikai-table-el5 td").height();
+  $('.joshikai-table .joshikai-table-el5 th').css('height', program_el2);
+
+  program_el6 = $(".joshikai-table .joshikai-table-el6 td").height();
+  $('.joshikai-table .joshikai-table-el6 th').css('height', program_el2);
+});
