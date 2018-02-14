740bf3043dafd3cfb95cf77a88b6d9a2e98b671f
toetheline03 <komori@sundaylunch.jp>
2018-02-14 19:35:39 +0900

    [master] ひみつの大女子会_調整

--- web/public/assets/js/kinyu/campaign/conference.js
+++ web/public/assets/js/kinyu/campaign/conference.js
@@ -6,14 +6,17 @@ $(function() {
   $('.joshikai-table .joshikai-table-el2 th').css('height', program_el2);
 
   program_el3 = $(".joshikai-table .joshikai-table-el3 td").height();
-  $('.joshikai-table .joshikai-table-el3 th').css('height', program_el2);
+  $('.joshikai-table .joshikai-table-el3 th').css('height', program_el3);
 
   program_el4 = $(".joshikai-table .joshikai-table-el4 td").height();
-  $('.joshikai-table .joshikai-table-el4 th').css('height', program_el2);
+  $('.joshikai-table .joshikai-table-el4 th').css('height', program_el4);
 
   program_el5 = $(".joshikai-table .joshikai-table-el5 td").height();
-  $('.joshikai-table .joshikai-table-el5 th').css('height', program_el2);
+  $('.joshikai-table .joshikai-table-el5 th').css('height', program_el5);
 
   program_el6 = $(".joshikai-table .joshikai-table-el6 td").height();
-  $('.joshikai-table .joshikai-table-el6 th').css('height', program_el2);
+  $('.joshikai-table .joshikai-table-el6 th').css('height', program_el6);
+
+  program_el7 = $(".joshikai-table .joshikai-table-el7 td").height();
+  $('.joshikai-table .joshikai-table-el7 th').css('height', program_el7);
 });
