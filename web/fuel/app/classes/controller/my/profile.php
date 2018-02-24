1f8749cb192d71a5721e5ecd988b1ebab561a27d
國井弘一 <koichi_kunii@bita.jp>
2018-02-24 18:28:14 +0900

    fix プロフィール編集でメールアドレスが変更されない不具合修正

--- web/fuel/app/classes/controller/my/profile.php
+++ web/fuel/app/classes/controller/my/profile.php
@@ -5,6 +5,7 @@ class Controller_My_Profile extends Controller_Mybase
 
     public function action_index()
     {
+        $this->template->email = Auth::get('email');
         $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
         $this->template->my_side = View::forge('my/common/my_side.smarty', $this->data);
         $this->template->contents = View::forge('my/profile/index.smarty', $this->data);
@@ -15,6 +16,7 @@ class Controller_My_Profile extends Controller_Mybase
 
     public function action_edit()
     {
+        $this->template->email = Auth::get('email');
         $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
         $this->template->my_side = View::forge('my/common/my_side.smarty', $this->data);
         $this->template->contents = View::forge('my/profile/edit.smarty', $this->data);
