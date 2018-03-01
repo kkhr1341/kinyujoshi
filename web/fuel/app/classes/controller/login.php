33af599facb173d32adea7468fa0e1a8416d8cd9
toetheline03 <komori@sundaylunch.jp>
2018-03-01 15:22:15 +0900

    既存メンバー用_パスワード設定フォーム

--- web/fuel/app/classes/controller/login.php
+++ web/fuel/app/classes/controller/login.php
@@ -139,6 +139,31 @@ class Controller_Login extends Controller_KinyuBase
         $this->template->contents = View::forge('login/resetting_pass.smarty', $this->data);
     }
 
+    public function action_resetting_pass_exuser()
+    {
+        // トークンチェック
+        if (!$reminder = UserReminder::get_valid(\Input::get('access_token'))) {
+            Response::redirect('/login');
+        }
+        $profile = Profiles::get($reminder['username']);
+        $this->template->nickname = $profile['nickname'];
+        $this->template->access_token = $reminder['access_token'];
+
+        $this->template->ogimg = 'https://kinyu-joshi.jp/images/kinyu-logo.png';
+        $this->template->title = 'パスワード再発行｜きんゆう女子。';
+        $this->template->description = 'パスワード再発行｜きんゆう女子';
+        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
+        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
+        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
+        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
+
+        if (Agent::is_mobiledevice()) {
+            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
+            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
+        }
+        $this->template->contents = View::forge('login/resetting_pass_exuser.smarty', $this->data);
+    }
+
     public function action_complete()
     {
         $this->template->title = 'メンバー登録ありがとうございます！｜きんゆう女子。';
