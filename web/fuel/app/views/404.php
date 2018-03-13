8e52b09273aeffe2cc2be926a588148905db3c55
toetheline03 <komori@sundaylunch.jp>
2018-03-13 19:08:06 +0900

    エラー画面の設定

--- web/fuel/app/views/404.php
+++ web/fuel/app/views/404.php
@@ -1,5 +1,264 @@
-404 not found
-<script>
-$(function(){
-});
-</script>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no,maximum-scale=1">
+		<title>きんゆう女子。- 金融ワカラナイ女子のためのコミュニティ</title>
+		<meta name="description" content="きんゆう女子。は、金融ワカラナイ女子のためのコミュニティです。なかなか聞けない、お金の話。 先延ばしにしがちな、お金の計画。 私には無関係と思っている、金融の話。みんなのお金に関するあれこれをおしゃべりしましょう！">
+		<meta name="copyright" content="Copyright &copy; きんゆう女子。 All Rights Reserved.">
+		<link type="image/x-icon" rel="shortcut icon" href="/images/favicon.png">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="apple-mobile-web-app-title" content="きんゆう女子。">
+		<meta property="og:image" content="https://kinyu-joshi.jp/images/og-top.png">
+
+		<link type="text/css" rel="stylesheet" href="/assets/css/kinyu/redactor.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/kinyu/toastr.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/kinyu/bootstrap01.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/kinyu/swiper.min.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/base.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/slick.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/kinyu/bg.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/edit_style.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/responsive.css" />
+		<link type="text/css" rel="stylesheet" href="/assets/css/tablet.css" />
+		<script type="text/javascript" src="/assets/js/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="/assets/js/moment.js"></script>
+		<script type="text/javascript" src="/assets/js/bootcards.min.js"></script>
+		<script type="text/javascript" src="/assets/js/bootbox.min.js"></script>
+		<script type="text/javascript" src="/assets/js/toastr.min.js"></script>
+		<script type="text/javascript" src="/assets/js/loader.js"></script>
+		<script type="text/javascript" src="/assets/js/ajax.js"></script>
+		<script type="text/javascript" src="/assets/js/redactor.js"></script>
+		<script type="text/javascript" src="/assets/js/payment.js?1520055221"></script>
+		<script type="text/javascript" src="/assets/js/imagemanager.js"></script>
+		<script type="text/javascript" src="/assets/js/filemanager.js"></script>
+		<script type="text/javascript" src="/assets/js/kinyu/slick.min.js"></script>
+		<script type="text/javascript" src="/assets/js/kinyu/instafeed.min.js"></script>
+		<script type="text/javascript" src="/assets/js/kinyu/main.js?1520073065"></script>
+		<script type="text/javascript" src="/assets/js/kinyu/swiper.min.js"></script>
+		<script type="text/javascript" src="/assets/js/kinyu/jquery.bxslider.min.js"></script>
+
+		<script type="text/javascript" src="//maps.google.com/maps/api/js?key=AIzaSyAFVmKPPwT4ziVB36CPISDQEQR-7c8lBoE"></script>
+		<meta name="google-site-verification" content="bt-Z1O8HWo_rQ3TlPedxm8q3q1xV9ky7hWlxAqisA2w" />
+		<link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+		<!-- <link href="https://fonts.googleapis.com/earlyaccess/mplus1p.css" rel="stylesheet" /> -->
+	</head>
+
+
+	<body class="drawer drawer--left">
+
+    <!-- PC ヘッダー -->
+		<header class="kinyu-header-top">
+		  <h1 class="main-logo"><a href="/"><img src="/images/kinyu-logo-top.png" alt="きんゆう女子。"></a></h1>
+		  <nav>
+		    <ul class="kinyu-header-nav">
+		      <li><a href="/news">ニュース</a></li>
+		      <li><a href="/joshikai">女子会</a></li>
+		      <li><a href="/report">レポート</a></li>
+		      <li><a href="/myway">「わたし」を知る。</a></li>
+		      <li><a href="/about">きん女。ストーリー</a></li>
+		      {if in_array('user', $roles) || in_array('admin', $roles)}
+		        <li><a href="/my">マイページ</a></li>
+		      {else}
+		        <li><a href="/login_regist">メンバー登録</a></li>
+		        <li><a href="/login">ログイン</a></li>
+		      {/if}
+		    </ul>
+		  </nav>
+		  <div class="pc">
+		    <div class="kinyu-header-social-list">
+		      <ul>
+		        <li><a href="https://www.instagram.com/kinyu_joshi/" target="_blank"><img src="/images/social/instagram_icon.png"></a></li>
+		        <li><a href="https://www.facebook.com/kinyujyoshi/" target="_blank"><img src="/images/social/fb_icon.png"></a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</header>
+
+  	<div class="kinyu-container">
+  		<div class="kinyu-main">
+
+  			<!-- ここにエラーのコンテンツが入る -->
+				<div class="kinyu-error-container">
+					<p>申し訳ありません...。<br>このページは存在しないようです(´・ω・`)</p>
+					<a href="/">トップページへ</a>
+				</div>
+
+  			<style>
+  			.kinyu-error-container {
+  				padding: 210px 0px 80px;
+  				background: #f7f7f7;
+  			}
+  			.kinyu-error-container p {
+  				font-size: 20px;
+  				line-height: 32px;
+  				text-align: center;
+  			}
+  			.kinyu-error-container a {
+  			  display: block;
+    			width: 200px;
+    			color: #fff;
+    			margin: 0 auto;
+    			margin-top: 24px;
+    			font-size: 13px;
+    			padding: 10px 0px;
+    			background: #8ddcd1;
+    			font-weight: bold;
+    			transition: .5s;
+    			text-align: center;
+    		}
+    		@media screen and (max-width: 1024px) {
+    			.kinyu-error-container {
+    				padding: 50px 0px 50px;
+    				background: #fff;
+    			}
+    			.kinyu-error-container p {
+  					font-size: 15px;
+  					line-height: 22px;
+  					padding: 0px 4%;
+  				}
+  				.kinyu-error-container a {
+  					font-size: 12px;
+  					padding: 8px 0px;
+  				}
+    		}
+  			</style>
+
+        <!-- フッター -->
+        <footer class="singlepage-footer">
+				  <img class="sp footer-icon" src="/images/new/footer-icon.png" class="footer-icon">
+				  <div class="footer-logo">
+				    <a href="/about"><img src="/images/kinyu-logo-top.png"></a>
+				  </div>
+				  <a class="sp navi-link-top" href="/">トップページへ</a>
+				  <div class="footer-menu">
+				    <div class="navi_kinyu-submenu-el clear">
+				      <a href="/company">運営会社</a>
+				    </div>
+				    <div class="navi_kinyu-submenu-el clear">
+				      <a href="/privacy">プライバシーポリシー</a>
+				    </div>
+				    <div class="navi_kinyu-submenu-el clear">
+				      <a href="/legal">特定商取引法に基づく表記</a>
+				    </div>
+				    <div class="navi_kinyu-submenu-el clear">
+				      <a href="/service">会員規約</a>
+				    </div>
+				    <div class="navi_kinyu-submenu-el clear">
+				      <a href="/contact">お問い合わせ</a>
+				    </div>
+				  </div>
+				  <p class="navi_kinyu-submenu-copy">
+				    <span>© TOE THE LINE Inc. 2018　All rights reserved.</span>
+				    掲載の写真・記事・イラスト等の全てのコンテンツの無断転写・転載を禁じます。
+				  </p>
+				</footer>
+
+  		</div>
+  	</div>
+
+
+  	<!-- SPナビゲーション -->
+		<div class="kinyu-sp-navi">
+		  <div class="kinyu-sp-header-menu kinyu-sp-header-close">
+		    <img class="menu-trigger" src="/images/new/sp-header-close_w.png">
+		    <a href="/" class="navi-icon"><img class="top-circle-logo" src="/images/top-circle-logo.png"></a>
+		  </div>
+
+		  <div class="inner">
+		    <p class="pc-menu-trigger-close"><img src="/images/new/sp-header-close.png"></p>
+		    <ul class="navi_contents-block-menu">
+		      <li class="item navi_kinyu-menu-li01">
+		        <div class="navi_kinyu-menu-block clear">
+		          <div class="navi_kinyu-menu-block-el navi-el-at">
+		            <a class="fc-kinyu" href="/about">きん女。ストーリー</a>
+		          </div>
+		          <div class="navi_kinyu-menu-block-el">
+		            <a href="/login_regist"><!-- <span style="color:#8ddcd1;">◆</span>  -->メンバー登録</a>
+		          </div>
+		          <div class="navi_kinyu-menu-block-el">
+		            <a href="/my"><!-- <span style="color:#8ddcd1;">◆</span>  -->マイページ</a>
+		          </div>
+		          <div class="navi_kinyu-menu-block-el">
+		            <a href="/joshikai"><!-- <span style="color:#8ddcd1;">◆</span>  -->女子会に参加する</a>
+		          </div>
+		          <div class="navi_kinyu-menu-block-el">
+		            <a href="/report"><!-- <span style="color:#8ddcd1;">◆</span>  -->レポートを見る</a>
+		          </div>
+		          <div class="navi_kinyu-menu-block-el">
+		            <a href="/news"><!-- <span style="color:#8ddcd1;">◆</span>  -->ニュースを見る</a>
+		          </div>
+		        </div>
+		      </li>
+
+		      <li class="item navi_kinyu-menu-li02">
+		        <div class="navi-social-area">
+		          <div class="navi-social-area-el">
+		            <a href="https://www.instagram.com/kinyu_joshi/" target="_blank"><img src="/images/social/instagram_icon.png"></a>
+		          </div>
+		          <div class="navi-social-area-el">
+		            <a href="https://www.facebook.com/kinyujyoshi/" target="_blank"><img src="/images/social/fb_icon.png"></a>
+		          </div>
+		        </div>
+		      </li>
+
+		      <li class="item navi_banner_area navi_kinyu-menu-li03">
+		        <a class="navi_banner_area_el" href="/myway">
+		          <img src="/images/myway/og_myway.jpg">
+		        </a>
+		      </li>
+
+		      <li class="item navi_kinyu-menu-li04">
+		      </li>
+		    </ul>
+
+		  </div>
+		</div>
+
+
+    <div class="sp">
+    	<!-- SP - ヘッダー -->
+			<header class="fix-header-sp">
+			  <a href="/" class="header-sp-logo"><img src="/images/kinyu-logo-sp.png"></a>
+			  <div class="kinyu-sp-header-menu">
+			    <img class="menu-trigger" src="/images/new/sp-header-menu.png">
+			  </div>
+			</header>
+
+			<div class="kinyu-sp-header">
+			  <div class="kinyu-sp-menu-block">
+			    <ul>
+			      <li class="menu-block-el1"><a href="/login_regist"><img src="/images/menu/under-menu-member.png"></a></li>
+			      <li class="menu-block-el2"><a href="/joshikai"><img src="/images/menu/under-menu-joshikai.png"></a></li>
+			      <li class="menu-block-el3"><a href="/my"><img src="/images/menu/under-menu-my.png"></a></li>
+			    </ul>
+			  </div>
+			</div>
+    </div>
+
+    <div class="loader-block">
+      <div class="loader-block-inner">
+        <div class="loader-el">
+          <div class="loader loader-1"></div>
+          <p>読み込み中...。</p>
+        </div>
+      </div>
+    </div>
+
+  	<!-- 解析コード -->
+  	<script type="text/javascript">
+  		var _gaq = _gaq || [];
+  		_gaq.push(['_setAccount', 'UA-72608536-2']);
+  		_gaq.push(['_trackPageview']);
+  		(function() {
+    		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  		})();
+		</script>
+
+  	<script src="/assets/js/kinyu/swiper.js"></script>
+  </body>
+
+</html>
