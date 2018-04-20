1d5bb8b291e58ec237b78295437af81b4bb98629
toetheline03 <komori@sundaylunch.jp>
2018-04-20 11:48:24 +0900

    fix

--- web/fuel/app/classes/controller/kinyu/campaign.php
+++ web/fuel/app/classes/controller/kinyu/campaign.php
@@ -154,6 +154,34 @@ class Controller_Kinyu_Campaign extends Controller_Kinyubase
         $this->template->contents = View::forge('kinyu/campaign/ooedoonsen_joshikai.smarty', $this->data);
     }
 
+    public function action_ooedojoshikai_2018()
+    {
+
+        switch (true) {
+            case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+            case $_SERVER['PHP_AUTH_USER'] !== 'ooedo2018':
+            case $_SERVER['PHP_AUTH_PW'] !== '1234567890':
+            header('WWW-Authenticate: Basic realm="Enter username and password."');
+            header('Content-Type: text/plain; charset=utf-8');
+            die('このページを見るにはログインが必要です');
+        }
+
+        $this->template->title = '【伊東編】大江戸温泉できんゆう女子。会';
+        $this->template->ogimg = 'https://kinyu-joshi.jp/images/content/ooedo_joshikai/oom-reit-og.jpg';
+        $this->template->description = '今回の特別女子会は、ついに実現！みんなで日帰り温泉で女子会♪テーマは、不動産投資信託（REIT：りーと）についてです。よくワカラナイままにしていた、投資信託についてもふれます。まずは、単語からお勉強しましょう！。';
+        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
+        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
+        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
+        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
+
+        if (Agent::is_mobiledevice()) {
+            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
+            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
+        }
+
+        $this->template->contents = View::forge('kinyu/campaign/ooedojoshikai_2018.smarty', $this->data);
+    }
+
     public function action_celebration_20171206()
     {
         $this->template->title = 'kinjo1000！Celebration｜きんゆう女子。';
