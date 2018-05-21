3cc53e0549e1f1d7357419a5bd2c99ad271805fa
國井弘一 <kuniiskywalker@gmail.com>
2018-05-21 16:14:42 +0900

    add きんゆうマップ

--- web/fuel/app/classes/controller/kinyu/campaign.php
+++ web/fuel/app/classes/controller/kinyu/campaign.php
@@ -116,6 +116,26 @@ class Controller_Kinyu_Campaign extends Controller_Kinyubase
         $this->template->contents = View::forge('kinyu/campaign/map.smarty', $this->data);
     }
 
+    public function action_kinyu_map()
+    {
+
+        $this->template->lang = 'en';
+        $this->template->title = 'かぶと町マップ｜きんゆう女子。';
+        $this->template->ogimg = 'https://kinyu-joshi.jp/images/map/map-og.jpg';
+        $this->template->description = 'レトロな町・金ゆうの町、日本橋兜町・茅場町。あまり知られていない、かくれ家的なお店がいっぱい！！ きんゆう女子。おすすめのスポットをご紹介♪';
+        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
+        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
+        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
+        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
+
+        if (Agent::is_mobiledevice()) {
+            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
+            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
+        }
+
+        $this->template->contents = View::forge('kinyu/campaign/kinyu_map.smarty', $this->data);
+    }
+
     public function action_kinyu_sanpo()
     {
 
