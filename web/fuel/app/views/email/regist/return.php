5d3b19a3381749fa88d03c0d19a0a5d3e45fe359
toetheline03 <komori@sundaylunch.jp>
2018-05-25 17:58:10 +0900

    メンバー登録通知の個人情報削除処理

--- web/fuel/app/views/email/regist/return.php
+++ web/fuel/app/views/email/regist/return.php
@@ -41,59 +41,7 @@
         <tr>
           <td>
             <h3>【きんゆう女子。】メンバー登録がありました！</h3>
-            <p>以下、メンバー情報です。</p>
-
-            <div class="form-mail-p">
-              <p>◼︎名前</p>
-              <?php echo $name; ?><br>
-              (ふりがな)<?php echo $name_kana; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎生年月日</p>
-              <?php echo $age; ?><br>
-              ＊正確な年齢は、<a href="https://kinyu-joshi.jp/my" target="_blank">管理ページ</a>からご確認ください。
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎きんゆうワカラナイ度</p>
-              <?php echo $not_know; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼お金について知りたいこと・興味のあること</p>
-              <?php echo $interest; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼きんゆう女子。でどんな出会いや発見がほしいですか？</p>
-              <?php echo $ask; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎3年後の自分の年収をどのくらいにしたいですか？ </p>
-              <?php echo $income; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎どこできんゆう女子。を知りましたか？</p>
-              <?php echo $where_from; ?><br>
-              <?php echo $where_from_other; ?>
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎きんゆう女子。で情報発信したいですか？</p>
-              <?php echo $transmission; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎メールアドレス</p>
-              <?php echo $email; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎FacebookのURL</p>
-              <?php echo $facebook; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎金融機関の方ですか？</p>
-              <?php echo $job_kind; ?><br>
-            </div>
-            <div class="form-mail-p">
-              <p>◼︎簡単な自己紹介</p>
-              <?php echo $introduction; ?><br>
-            </div>
+            <p>メンバーの詳細は、<a href="https://kinyu-joshi.jp/admin" target="_blank">管理ページ</a>からご確認ください。</p>
 
             <style>
               .form-mail-p {
