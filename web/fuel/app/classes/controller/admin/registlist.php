790337cffcdd120abe3705304d8bc8504214b4ed
國井弘一 <kuniiskywalker@gmail.com>
2018-06-25 19:30:25 +0900

    update 管理画面会員リストに検索機能追加

--- web/fuel/app/classes/controller/admin/registlist.php
+++ web/fuel/app/classes/controller/admin/registlist.php
@@ -19,14 +19,26 @@ class Controller_Admin_Registlist extends Controller_Adminbase
 
     public function action_index()
     {
-
         $this->data['sections'] = Sections::lists();
-        $this->data['registlist'] = Regist::lists();
-        //print_r($this->data['registlist']);
+
+        $params = Input::get();
+        unset($params['page']);
+
+        $this->data['registlist'] = Regist::all('/admin/registlist/?' . http_build_query($params), Input::get('page', 1), 'page', 30, $params);
+        $pagination = $this->data['registlist']['pagination'];
+
+        $this->data['params'] = array(
+            'username' => Input::get('username', ''),
+            'name' => Input::get('name', ''),
+            'email' => Input::get('email', ''),
+            'introduction' => Input::get('introduction', ''),
+        );
+
+        $this->template->total = Regist::total();
         $this->template->contents = View::forge('admin/registlist/registlist.smarty', $this->data);
+        $this->template->contents->set_safe('pagination', $pagination);
         $this->template->description = 'マイページ・ブログ';
         $this->template->ogimg = 'https://kinyu-joshi.jp/images/kinyu-logo.png';
-        //$this->template->diary_bottom = View::forge('fitthelocal/triparea/template/hawaii_bottom.smarty', $this->data);
     }
 
     public function action_detail($code)
