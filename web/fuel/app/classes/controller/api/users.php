e0d1d0c36a1c4a44f72377e316c7f269bfcf02ec
國井弘一 <kuniiskywalker@gmail.com>
2018-07-06 13:56:02 +0900

    add メアド＆パスワード変更機能

--- web/fuel/app/classes/controller/api/users.php
+++ web/fuel/app/classes/controller/api/users.php
@@ -1,9 +1,32 @@
 <?php
 
+use \Model\User;
 use \Model\Userwithdrawal;
 
 class Controller_Api_Users extends Controller_Base
 {
+
+    public function action_save()
+    {
+        if (!Auth::check()) {
+            \Session::set('referrer', \Input::referrer());
+            return $this->ok('login');
+        }
+        $val = User::validate();
+        if (!$val->run()) {
+            $error_messages = $val->error_message();
+            $message = reset($error_messages);
+            return $this->error($message);
+        }
+        $params = $val->validated();
+        $res = User::save($params);
+        if (is_string($res)) {
+            return $this->error($res);
+        }
+
+        return $this->ok();
+    }
+
     /**
      * ログイン
      */
