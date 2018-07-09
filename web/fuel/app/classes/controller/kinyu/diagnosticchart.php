93a08aade565d764a77f9ceb70125396c6ce7639
國井弘一 <kuniiskywalker@gmail.com>
2018-07-09 10:19:39 +0900

    fix 診断チャート完了画面を別ページに変更

--- web/fuel/app/classes/controller/kinyu/diagnosticchart.php
+++ web/fuel/app/classes/controller/kinyu/diagnosticchart.php
@@ -20,4 +20,22 @@ class Controller_Kinyu_Diagnosticchart extends Controller_Kinyubase
         }
         $this->template->contents = View::forge('kinyu/diagnosticchart/index.smarty', $this->data);
     }
+
+    public function action_complete($code)
+    {
+        $this->template->title = '女子会ページ｜きんゆう女子。';
+        $this->template->description = "きんゆう女子。では、お金に関する様々な情報を学ぶことができるイベントを開催しています。みなさんでお金に関するあれこれをたくさんおしゃべりしましょう！";
+        $this->template->ogimg = 'https://kinyu-joshi.jp/images/og-jyoshikai.jpg';
+        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
+        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
+        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
+
+        if (Agent::is_mobiledevice()) {
+            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
+            $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
+        } else {
+            $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
+        }
+        $this->template->contents = View::forge('kinyu/diagnosticchart/complete.smarty', $this->data);
+    }
 }
