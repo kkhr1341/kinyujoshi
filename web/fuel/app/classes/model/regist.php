11d5e3778e01920b81bcb9ecf0c721e00ad9728b
國井弘一 <kuniiskywalker@gmail.com>
2018-08-06 14:34:45 +0900

    fix

--- web/fuel/app/classes/model/regist.php
+++ web/fuel/app/classes/model/regist.php
@@ -276,7 +276,10 @@ class Regist extends Base
             $total->where('member_regist.introduction', 'like', '%' . $params['introduction'] . '%');
         }
         if (isset($params['group']) && $params['group']) {
-            $total->where('users.group', 'in', $params['group']);
+            $total->or_where_open()
+                ->where('users.group', 'in', $params['group'])
+                ->where('users.group', 'is', null)
+                ->or_where_close();
         }
 
         $total = $total->execute()->current();
@@ -343,7 +346,10 @@ class Regist extends Base
             $datas['datas']->where('member_regist.introduction', 'like', '%' . $params['introduction'] . '%');
         }
         if (isset($params['group']) && $params['group']) {
-            $datas['datas']->where('users.group', 'in', $params['group']);
+            $datas['datas']->or_where_open()
+                ->where('users.group', 'in', $params['group'])
+                ->where('users.group', 'is', null)
+                ->or_where_close();
         }
 
         $datas['datas'] = $datas['datas']->limit($pagination->per_page)
