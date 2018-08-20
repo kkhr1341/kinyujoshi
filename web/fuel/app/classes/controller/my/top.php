e6d5b877e354b3bf0fc3d519a67899e6828fa439
toetheline03 <komori@sundaylunch.jp>
2018-08-20 10:57:47 +0900

    fix

--- web/fuel/app/classes/controller/my/top.php
+++ web/fuel/app/classes/controller/my/top.php
@@ -17,10 +17,7 @@ class Controller_My_Top extends Controller_Mybase
         $this->template->contents = View::forge('my/all.smarty', $this->data);
         $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
         $this->template->ogimg = 'https://kinyu-joshi.jp/images/kinyu-logo.png';
-        if (Agent::is_mobiledevice()) {
-            $this->template->kinjo_check = View::forge('my/common/kinjo_check.smarty', $this->data);
-        } else {
-            $this->template->kinjo_check = View::forge('my/common/kinjo_check_sp.smarty', $this->data);
-        }
+        $this->template->kinjo_check = View::forge('my/common/kinjo_check.smarty', $this->data);
+
     }
 }
