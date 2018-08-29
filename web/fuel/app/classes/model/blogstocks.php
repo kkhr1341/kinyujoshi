b5da9ebe3397efaad4f88774edb0e22cf0766985
toetheline03 <komori@sundaylunch.jp>
2018-08-29 14:49:14 +0900

    fix

--- web/fuel/app/classes/model/blogstocks.php
+++ web/fuel/app/classes/model/blogstocks.php
@@ -42,13 +42,16 @@ class Blogstocks extends Base
 
     public static function lists()
     {
+        $username = \Auth::get('username');
         return \DB::select(\DB::expr('blogs.*'))
             ->from('blogs')
             ->join('blog_stocks')
             ->on('blogs.code', '=', 'blog_stocks.blog_code')
+            ->where('blog_stocks.username', '=', $username)
             ->where('blogs.status', '=', 1)
             ->where('blogs.disable', '=', 0)
             ->execute()
             ->as_array();
     }
+
 }
