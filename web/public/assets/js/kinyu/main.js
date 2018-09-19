12313f3fdb28943af0a467bff8a583c390f9ff43
toetheline03 <komori@sundaylunch.jp>
2018-09-19 11:59:10 +0900

    fix

--- web/public/assets/js/kinyu/main.js
+++ web/public/assets/js/kinyu/main.js
@@ -632,7 +632,7 @@ $(document).ready(function() {
     limit: 9, //取得する画像数を設定
     resolution: 'standard_resolution', //画像サイズを設定
     template: '<li><a href="{{link}}" target="_blank" style="background: url({{image}}) center center / cover no-repeat;"><p><span>❤︎ {{likes}}</span></p></a></li>',
-    accessToken: '3007140595.196d16b.a5a24b6446904e8da973b17ae9f25e21' //アクセストークン(ローカル)
+    accessToken: '3007140595.5185c52.5cd9a9c66771427cbb5e262168dcb54a' //アクセストークン(ローカル)
   });
   userFeed.run();
 });
