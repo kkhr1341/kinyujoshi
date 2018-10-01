f3260b7e88d7f9dab4e721046fb7b1bfe76076fe
國井弘一 <kuniiskywalker@gmail.com>
2018-10-01 17:05:12 +0900

    add トップページのロードアニメーション無効

--- web/public/assets/js/kinyu/topanimation.js
+++ web/public/assets/js/kinyu/topanimation.js
@@ -1,6 +1,19 @@
 //ページ読み込み時のアニメーション
 $(document).ready(function(){
 
+  var today = new Date();
+
+  var topAnimationDisplayTime = document.cookie.replace(/(?:(?:^|.*;\s*)top_animation_display_time\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  topAnimationDisplayTime = parseInt(topAnimationDisplayTime, 10) + (60 * 60 * 24 * 1000);
+
+  if (today.getTime() < topAnimationDisplayTime) {
+      $("#kinyu-main-animation-inner").hide();
+      $("#kinyu-main-animation").hide();
+      return;
+  }
+
+  document.cookie = "top_animation_display_time=" + today.getTime();
+
   var state = false;
   var scrollpos;
 
