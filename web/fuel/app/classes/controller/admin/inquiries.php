22375116b157c82b4cd26ba24da3796a44d9e01d
國井弘一 <kuniiskywalker@gmail.com>
2018-12-17 18:54:10 +0900

    add お問い合わせ管理ページにIP制限を追加

--- web/fuel/app/classes/controller/admin/inquiries.php
+++ web/fuel/app/classes/controller/admin/inquiries.php
@@ -7,7 +7,7 @@ class Controller_Admin_Inquiries extends Controller_Adminbase
 
     public function action_index()
     {
-        if (!Auth::has_access('inquiries.read')) {
+        if (!Auth::has_access('inquiries.read') || !$this->is_from_company()) {
             throw new HttpNoAccessException;
         }
         $this->data['inquiries'] = Inquiries::lists();
