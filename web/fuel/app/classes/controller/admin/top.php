7f0fc303f4b87d9a584881f6fcd1c273535c8f96
國井弘一 <kuniiskywalker@gmail.com>
2019-01-18 11:59:50 +0900

    clean 管理画面のトップページテンプレートを移動

--- web/fuel/app/classes/controller/admin/top.php
+++ web/fuel/app/classes/controller/admin/top.php
@@ -12,7 +12,7 @@ class Controller_Admin_Top extends Controller_Adminbase
         }
         $this->template->header = View::forge('kinyu/template/header-area.smarty', $this->data);
         $this->template->footer = View::forge('kinyu/template/footer-area.smarty', $this->data);
-        $this->template->contents = View::forge('admin/index.smarty', $this->data);
+        $this->template->contents = View::forge('admin/top/index.smarty', $this->data);
         $this->template->ogimg = 'https://kinyu-joshi.jp/images/kinyu-logo.png';
         $this->template->title = 'マイページ｜きんゆう女子。';
         $this->template->description = 'マイページ・トップ';
