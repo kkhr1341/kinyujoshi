0d85aab9e559b034129bfbdc5f2aeaeda8e26ef9
國井弘一 <kuniiskywalker@gmail.com>
2019-01-21 19:02:40 +0900

    change sitemap limit

--- web/fuel/app/classes/controller/kinyu/sitemap.php
+++ web/fuel/app/classes/controller/kinyu/sitemap.php
@@ -75,7 +75,7 @@ class Controller_Kinyu_SiteMap extends Controller_Rssbase
          );
 
         // レポート
-        $blogs = Blogs::lists(1, null, 1);
+        $blogs = Blogs::lists(1, 20, 1);
         foreach ($blogs as $blog) {
             array_push($site_data, array(
                 'loc'=>'https://kinyu-joshi.jp/report/' . $blog['code'],
@@ -86,7 +86,7 @@ class Controller_Kinyu_SiteMap extends Controller_Rssbase
         }
 
         // 女子会
-        $events = Events::lists(1, null, 1);
+        $events = Events::lists(1, 20, 1);
         foreach ($events as $event) {
             array_push($site_data, array(
                 'loc'=>'https://kinyu-joshi.jp/joshikai/' . $event['code'],
