80b08cda13e7679a5f8adf605080d3c2a9e51bee
EC2 Default User <ec2-user@ip-172-31-24-237.ap-northeast-1.compute.internal>
2019-05-17 02:29:53 +0000

    fix top page

--- web/fuel/app/classes/controller/kinyu/top.php
+++ web/fuel/app/classes/controller/kinyu/top.php
@@ -12,8 +12,9 @@ class Controller_Kinyu_Top extends Controller_Kinyubase
 
     public function action_index($page = 1)
     {
-        $this->data['projects'] = Projects::lists(1, 3, true, 'kinyu');
+        // $this->data['projects'] = Projects::lists(1, 3, true, 'kinyu');
         $this->data['news'] = News::lists(1, 1, true);
+/*
         if (Agent::is_mobiledevice()) {
             $this->data['blogs'] = Blogs::all('kinyu' + 'investment', '/report/', $page, 2, 20);
         } else {
@@ -21,6 +22,7 @@ class Controller_Kinyu_Top extends Controller_Kinyubase
         }
         $pagination = $this->data['blogs']['pagination'];
         $this->data['pagination'] = $pagination::instance('mypagination');
+*/
 
         $this->data['event_display_limit'] = self::EVENT_DISPLAY_LIMIT;
         $this->data['events'] = Events::lists(1, self::EVENT_DISPLAY_LIMIT, true, 0, 'asc');
