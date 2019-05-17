bfc2a31a51031922038aa625c8ab2403632e93c6
國井弘一 <kuniiskywalker@gmail.com>
2019-05-17 15:04:02 +0900

    fix メンバー投稿

---
+++ web/fuel/app/classes/controller/my/author.php
@@ -0,0 +1,25 @@
+<?php
+
+use \Model\DiagnosticChartTypeUsers;
+use \Model\DiagnosticChartRouteTypeHashTags;
+use \Model\DiagnosticChartRouteTypeActionLists;
+
+class Controller_My_Author extends Controller_Mybase
+{
+
+    public function action_edit()
+    {
+        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
+        $username = \Auth::get('username');
+        $user_type = DiagnosticChartTypeUsers::getLastUserType($username);
+
+        $this->template->title = 'お気に入り｜きん女。マイページ';
+        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
+        $this->template->contents = View::forge('my/author/index.smarty', $this->data);
+        $this->template->my_side = View::forge('my/common/my_side.smarty', array(
+            'user_type' => $user_type,
+            'hash_tags' => DiagnosticChartRouteTypeHashTags::getTagsByTypeCode($user_type['type_code']),
+            'action_list' => DiagnosticChartRouteTypeActionLists::getContentByTypeCode($user_type['type_code']),
+        ));
+    }
+}
--- web/fuel/app/classes/controller/my/reports.php
+++ web/fuel/app/classes/controller/my/reports.php
@@ -1,7 +1,5 @@
 <?php
 
-use \Model\Blogs;
-use \Model\Blogstocks;
 use \Model\DiagnosticChartTypeUsers;
 use \Model\DiagnosticChartRouteTypeHashTags;
 use \Model\DiagnosticChartRouteTypeActionLists;
@@ -11,29 +9,33 @@ class Controller_My_Reports extends Controller_Mybase
 
     public function action_index()
     {
-//        $username = \Auth::get('username');
         $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
-//        $username = \Auth::get('username'); //追加
-//        $this->data['blogs'] = Blogstocks::lists($username);
-//        $this->template->ogimg = 'https://kinyu-joshi.jp/images/kinyu-logo.png';
-//        $this->template->description = '女子会リスト';
-
-        Asset::css(array(
-            'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.css'
-        ), array(), 'layout', false);
-
-        Asset::js(array(
-            'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js'
-        ), array(), 'layout', false);
+        $username = \Auth::get('username');
+        $user_type = DiagnosticChartTypeUsers::getLastUserType($username);
 
         $this->template->title = 'お気に入り｜きん女。マイページ';
         $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
         $this->template->contents = View::forge('my/reports/index.smarty', $this->data);
-//        $user_type = DiagnosticChartTypeUsers::getLastUserType($username);
-//        $this->template->my_side = View::forge('my/common/my_side.smarty', array(
-//            'user_type' => $user_type,
-//            'hash_tags' => DiagnosticChartRouteTypeHashTags::getTagsByTypeCode($user_type['type_code']),
-//            'action_list' => DiagnosticChartRouteTypeActionLists::getContentByTypeCode($user_type['type_code']),
-//        ));
+        $this->template->my_side = View::forge('my/common/my_side.smarty', array(
+            'user_type' => $user_type,
+            'hash_tags' => DiagnosticChartRouteTypeHashTags::getTagsByTypeCode($user_type['type_code']),
+            'action_list' => DiagnosticChartRouteTypeActionLists::getContentByTypeCode($user_type['type_code']),
+        ));
+    }
+
+    public function action_edit()
+    {
+        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
+        $username = \Auth::get('username');
+        $user_type = DiagnosticChartTypeUsers::getLastUserType($username);
+
+        $this->template->title = 'お気に入り｜きん女。マイページ';
+        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
+        $this->template->contents = View::forge('my/reports/index.smarty', $this->data);
+        $this->template->my_side = View::forge('my/common/my_side.smarty', array(
+            'user_type' => $user_type,
+            'hash_tags' => DiagnosticChartRouteTypeHashTags::getTagsByTypeCode($user_type['type_code']),
+            'action_list' => DiagnosticChartRouteTypeActionLists::getContentByTypeCode($user_type['type_code']),
+        ));
     }
 }
