755d26a386b366cbca72fe0b69df2647064467aa
toetheline03 <komori@sundaylunch.jp>
2019-06-18 11:34:56 +0900

    fix

---
+++ web/public/assets/js/kinyu/scrollanimation.js
@@ -0,0 +1,28 @@
+ScrollReveal().reveal('.top-slide', { easing: 'ease', origin: 'bottom', duration: 2000, distance: '80px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.top-slide-delay', { easing: 'ease', origin: 'bottom', delay: 500, duration: 2000, distance: '80px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.left-slide01', { easing: 'ease', origin: 'left', delay: 500 , duration:  1500, distance: '100px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.right-slide01', { easing: 'ease', origin: 'right', duration:  1500, distance: '150px', opacity: 0, scale: 1 });
+
+ScrollReveal().reveal('.left-slide02', { easing: 'ease', origin: 'right', delay: 500 , duration:  1500, distance: '100px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.right-slide02', { easing: 'ease', origin: 'left', duration:  1500, distance: '150px', opacity: 0, scale: 1 });
+
+ScrollReveal().reveal('.single-title-slide', { easing: 'ease', origin: 'left', delay: 0 , duration:  1500, distance: '100px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.single-description-slide', { easing: 'ease', origin: 'left', delay: 1000 , duration:  1500, distance: '100px', opacity: 0, scale: 1 });
+
+
+//TOP - 背景部分
+ScrollReveal().reveal('.logo-animation', { easing: 'ease', origin: 'bottom', duration: 3000, distance: '0px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.intro-bg1', { easing: 'ease', origin: 'bottom', delay: 300, duration: 2500, distance: '0px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.intro-bg2', { easing: 'ease', origin: 'bottom', delay: 600, duration: 2500, distance: '0px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.intro-bg3', { easing: 'ease', origin: 'bottom', delay: 900, duration: 2500, distance: '0px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.intro-bg4', { easing: 'ease', origin: 'bottom', delay: 1200, duration: 2500, distance: '0px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.intro-title-animation', { easing: 'ease', origin: 'bottom', delay: 1500, duration: 3000, distance: '5px', opacity: 0, scale: 1 });
+
+//message-main
+ScrollReveal().reveal('.message-animation1', { easing: 'ease', origin: 'bottom', duration: 2500, distance: '30px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.message-animation2', { easing: 'ease', origin: 'left', delay: 500, duration: 2500, distance: '30px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.message-animation3', { easing: 'ease', origin: 'right', delay: 300, duration: 3000, distance: '50px', opacity: 0, scale: 1 });
+ScrollReveal().reveal('.message-animation4', { easing: 'ease', origin: 'right', delay: 800, duration: 3000, distance: '50px', opacity: 0, scale: 1 });
+
+//キャッシュレス
+ScrollReveal().reveal('.cashless-animation1', { easing: 'ease', origin: 'bottom', duration: 3000, distance: '50px', opacity: 0, scale: 1 });
