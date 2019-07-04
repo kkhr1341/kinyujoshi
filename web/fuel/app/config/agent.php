428ec7221375adad7a9a111b7e313d07a4997a32
EC2 Default User <ec2-user@ip-172-31-24-237.ap-northeast-1.compute.internal>
2019-07-04 03:35:20 +0000

    fix browscap

---
+++ web/fuel/app/config/agent.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'browscap' => array(
+        'enabled' => true,
+        'url' => 'http://browscap.org/stream?q=Lite_PHP_BrowsCapINI',
+        'method' => 'local',
+        'file' => APPPATH . 'config/lite_php_browscap.ini',
+    ),
+);
+?>
