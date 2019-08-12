cf56bb19059516d2c00e23409072b222bdc62292
國井弘一 <kuniiskywalker@gmail.com>
2019-08-12 14:04:06 +0900

    change gaのプロパティ変更

--- web/fuel/app/views/parts/ga.php
+++ web/fuel/app/views/parts/ga.php
@@ -1,11 +1,11 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-72608536-2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-72608536-11"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-72608536-2', {
+  gtag('config', 'UA-72608536-11', {
       'custom_map': {
           'dimension1': 'clientId',
           'dimension2': 'secretBlog',
