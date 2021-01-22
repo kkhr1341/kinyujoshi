9caa81eca8bfe60a879f77cd01688d20474eb686
玉木大道 <tamaki@sundaylunch.jp>
2021-01-22 22:48:23 +0900

    agent.phpを編集

--- web/fuel/app/classes/agent.php
+++ web/fuel/app/classes/agent.php
@@ -4,27 +4,14 @@ class Agent extends Fuel\Core\Agent
     public static function _init()
     {
         parent::_init();
- 
-        $sp_list = array(
-                       'iPhone',
-                       'iPod',
-                       'ipad',
-                       'Android',
-                       'IEMobile',
-                       'dream',
-                       'CUPCAKE',
-                       'blackberry9500',
-                       'blackberry9530',
-                       'blackberry9520',
-                       'blackberry9550',
-                       'blackberry9800',
-                       'webOS',
-                       'incognito',
-                       'webmate'
-                   );
- 
-        $pattern = '/'.implode('|', $sp_list).'/i';
-        static::$properties['x_issmartphone'] = preg_match($pattern, static::$user_agent) ? true : false;
+        if (
+            isset($_SERVER['HTTP_CLOUDFRONT_IS_MOBILE_VIEWER']) &&
+            $_SERVER['HTTP_CLOUDFRONT_IS_MOBILE_VIEWER'] == 'true'
+        ) {
+            static::$properties['x_issmartphone'] = true;
+        } else {
+            static::$properties['x_issmartphone'] = false;
+        }
     }
  
     public static function is_smartphone()
