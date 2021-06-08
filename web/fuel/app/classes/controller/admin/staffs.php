3538f2b673a27a9313082bb08e427d3142fd8264
國井弘一 <kuniiskywalker@gmail.com>
2021-06-08 17:34:36 +0900

    feat 管理画面の権限周り調整

--- web/fuel/app/classes/controller/admin/staffs.php
+++ web/fuel/app/classes/controller/admin/staffs.php
@@ -34,12 +34,14 @@ class Controller_Admin_Staffs extends Controller_Adminbase
 
     $this->data['staff'] = User::staff_detail($user_id);
     $this->data['grants'] = array(
-      array('code' => -1, 'name' => 'Banned'),
+      array('code' => -1, 'name' => 'Banned（削除）'),
       array('code' => 1, 'name' => 'メンバー'),
-      array('code' => 30, 'name' => 'オフィシャルメンバー'),
-      array('code' => 40, 'name' => 'コミュニティスタッフ'),
-      array('code' => 80, 'name' => '管理者'),
-      array('code' => 100, 'name' => 'スーパー管理者')
+      array('code' => 30, 'name' => '（旧）オフィシャルメンバー（削除）'),
+      array('code' => 40, 'name' => '編集部'),
+      array('code' => 41, 'name' => '(新）オフィシャルメンバー'),
+      array('code' => 42, 'name' => '法人'),
+      array('code' => 80, 'name' => '管理者（削除）'),
+      array('code' => 100, 'name' => '社員・事務局')
     );
     $this->template->contents = View::forge('admin/staffs/edit.smarty', $this->data);
     $this->template->description = '管理画面・スタッフ詳細';
