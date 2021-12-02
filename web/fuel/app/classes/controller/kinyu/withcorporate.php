54852b683ab2ed15e9ac1c77bf03d1e633c9a3aa
komokei <komori@toetheline.jp>
2021-12-02 14:10:11 +0900

    fix

--- web/fuel/app/classes/controller/kinyu/withcorporate.php
+++ web/fuel/app/classes/controller/kinyu/withcorporate.php
@@ -19,16 +19,8 @@ class Controller_Kinyu_Withcorporate extends Controller_Kinyubase
         $this->template->ogimg = 'https://kinyu-joshi.jp/images/og-cashless.png';
         $this->template->description = 'きんゆう女子。は、金融ワカラナイ女子のためのコミュニティです。Aboutページでは、きんゆう女子。についての説明をしています。';
 
-        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
-        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
-        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
-        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-
-        if (Agent::is_mobiledevice()) {
-            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
-        } else {
-        }
+        $this->template->header = View::forge('kinyu/common/header.smarty', $this->data);
+        $this->template->footer = View::forge('kinyu/common/footer.smarty', $this->data);
         $this->template->contents = View::forge('kinyu/withcorporate/paypay.smarty', $this->data);
     }
 
@@ -48,16 +40,8 @@ class Controller_Kinyu_Withcorporate extends Controller_Kinyubase
         $this->template->ogimg = 'https://kinyu-joshi.jp/images/withcorporate/paypay/article/article1_og.jpg';
         $this->template->description = 'きんゆう女子。は、金融ワカラナイ女子のためのコミュニティです。Aboutページでは、きんゆう女子。についての説明をしています。';
 
-        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
-        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
-        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
-        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-
-        if (Agent::is_mobiledevice()) {
-            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
-        } else {
-        }
+        $this->template->header = View::forge('kinyu/common/header.smarty', $this->data);
+        $this->template->footer = View::forge('kinyu/common/footer.smarty', $this->data);
         $this->template->contents = View::forge('kinyu/withcorporate/article01.smarty', $this->data);
     }
 
@@ -77,16 +61,8 @@ class Controller_Kinyu_Withcorporate extends Controller_Kinyubase
         $this->template->ogimg = '/images/withcorporate/paypay/article02/article2_og.jpg';
         $this->template->description = 'きんゆう女子。は、金融ワカラナイ女子のためのコミュニティです。Aboutページでは、きんゆう女子。についての説明をしています。';
 
-        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
-        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
-        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
-        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-
-        if (Agent::is_mobiledevice()) {
-            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
-        } else {
-        }
+        $this->template->header = View::forge('kinyu/common/header.smarty', $this->data);
+        $this->template->footer = View::forge('kinyu/common/footer.smarty', $this->data);
         $this->template->contents = View::forge('kinyu/withcorporate/article02.smarty', $this->data);
     }
 
@@ -106,16 +82,8 @@ class Controller_Kinyu_Withcorporate extends Controller_Kinyubase
         $this->template->ogimg = '/images/withcorporate/paypay/article03/article3_og.jpg';
         $this->template->description = 'きんゆう女子。は、金融ワカラナイ女子のためのコミュニティです。Aboutページでは、きんゆう女子。についての説明をしています。';
 
-        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
-        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
-        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
-        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-
-        if (Agent::is_mobiledevice()) {
-            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
-        } else {
-        }
+        $this->template->header = View::forge('kinyu/common/header.smarty', $this->data);
+        $this->template->footer = View::forge('kinyu/common/footer.smarty', $this->data);
         $this->template->contents = View::forge('kinyu/withcorporate/article03.smarty', $this->data);
     }
 
@@ -135,16 +103,8 @@ class Controller_Kinyu_Withcorporate extends Controller_Kinyubase
         $this->template->ogimg = '/images/withcorporate/paypay/article04/article4_og.jpg';
         $this->template->description = 'きんゆう女子。は、金融ワカラナイ女子のためのコミュニティです。Aboutページでは、きんゆう女子。についての説明をしています。';
 
-        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
-        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
-        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
-        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-
-        if (Agent::is_mobiledevice()) {
-            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
-        } else {
-        }
+        $this->template->header = View::forge('kinyu/common/header.smarty', $this->data);
+        $this->template->footer = View::forge('kinyu/common/footer.smarty', $this->data);
         $this->template->contents = View::forge('kinyu/withcorporate/article04.smarty', $this->data);
     }
 
@@ -164,16 +124,8 @@ class Controller_Kinyu_Withcorporate extends Controller_Kinyubase
         $this->template->ogimg = '/images/withcorporate/paypay/article05/article5_og.jpg';
         $this->template->description = 'きんゆう女子。は、金融ワカラナイ女子のためのコミュニティです。Aboutページでは、きんゆう女子。についての説明をしています。';
 
-        $this->template->sp_header = View::forge('kinyu/common/sp_header.smarty', $this->data);
-        $this->template->pc_header = View::forge('kinyu/common/pc_header.smarty', $this->data);
-        $this->template->sp_footer = View::forge('kinyu/common/sp_footer.smarty', $this->data);
-        $this->template->sp_navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-
-        if (Agent::is_mobiledevice()) {
-            $this->template->navigation = View::forge('kinyu/common/sp_navigation.smarty', $this->data);
-            $this->template->sp_top_after = View::forge('kinyu/common/sp_top_after.smarty', $this->data);
-        } else {
-        }
+        $this->template->header = View::forge('kinyu/common/header.smarty', $this->data);
+        $this->template->footer = View::forge('kinyu/common/footer.smarty', $this->data);
         $this->template->contents = View::forge('kinyu/withcorporate/article05.smarty', $this->data);
     }
 }
